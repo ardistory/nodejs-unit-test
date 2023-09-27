d921d1adad2cfa293f9c92adfcca475d755935e1
Ardiansyah Putra <putraardiansyah530@gmail.com>
2023-09-27 19:53:23 +0700

    nodejs unit-test

---
+++ src/sum.test.js
@@ -0,0 +1,14 @@
+import { sum } from "./sum.js";
+
+test('test function sum', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test('test function sum2', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test('test function sum3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test('test function sum4', () => {
+    expect(sum(1, 2)).toBe(3);
+});
